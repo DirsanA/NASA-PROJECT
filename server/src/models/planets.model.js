0363fa191b08a26a7cccea75af065ae1e6f1d418
DirsanA <dirsanantehun739@gmail.com>
2025-01-03 00:32:44 -0800

    plane model designed from nasa file extracted

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,45 @@
-const planets = [];
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
+const habitablePlanets = [];
 
-module.exports = planets;
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+function loadPlanetData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.error("Error reading the file:", err);
+        reject(err); // Reject the promise on error
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve(habitablePlanets); // Resolve the promise on completion
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetData, // Exporting the function to load data
+  planets: habitablePlanets, // Exporting the array of planets
+};
--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,18 @@ const http = require("http");
 
 const app = require("./app");
 
+const { loadPlanetData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}...`);
-});
+async function startServer() {
+  await loadPlanetData();
+
+  server.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
